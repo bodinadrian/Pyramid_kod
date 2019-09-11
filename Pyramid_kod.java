696444004b6eb124a52ff012dfe52fc8a41471d5
bodinadrian <54884685+bodinadrian@users.noreply.github.com>
2019-09-11 11:22:23 +0200

    Create Pyramid_kod.java

---
+++ Pyramid_kod.java
@@ -0,0 +1,56 @@
+package Package_1;
+
+import javax.swing.*;
+
+public class Konvertera_C_Kod {
+
+public static void main(String [] args){
+
+int i, j, k;
+
+int empty_spaces;
+int number_of_rows = 0; //heltals variabel
+
+boolean go=true;
+
+while(go){ //while-loop START
+
+String input1 = JOptionPane.showInputDialog("How many rows of '#' do you want?\n");
+number_of_rows = Integer.parseInt(input1);
+
+if(number_of_rows==23){
+JOptionPane.showMessageDialog(null, "Wrong number of rows, please fill in again");
+go=true;
+}
+
+if(number_of_rows>0 && number_of_rows<=23){
+go=false;
+}
+
+} //while-loop END
+
+empty_spaces= number_of_rows;
+
+System.out.println("Height: " + number_of_rows);
+
+for(i=2; i<=number_of_rows+1; i++){ //For each row
+    
+for(j=1; j<empty_spaces; j++){ 
+System.out.print(" "); //Fill empty spaces in each row
+}
+
+empty_spaces--; //Decrease empty spaces in each row
+
+for(k=1; k<=i; k++){ //Fill with "#" for each row
+System.out.print("# ");
+}
+ 
+System.out.println();
+
+}
+
+System.exit(0);
+
+}
+
+}
